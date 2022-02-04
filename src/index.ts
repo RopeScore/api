58dd7317a262e2cd7902d28f87d4a00e4ba072a9
Svante Bengtson <svante@swantzter.se>
2022-02-04 15:49:56 +0000

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-// import 'pino-debug'
 import { PORT } from './config'
 import { initApollo } from './apollo'
 import { logger } from './services/logger'
 import express from 'express'
+import cors from 'cors'
 import http from 'http'
-// import { GRAPHQL_WS } from 'subscriptions-transport-ws'
-// import { GRAPHQL_TRANSPORT_WS_PROTOCOL } from 'graphql-ws'
 
 const app = express()
 const httpServer = http.createServer(app)
+
+app.use(cors({
+  origin: [
+    /(^https?:\/\/|\.)ropescore\.(com|app|live)(:\d+)?$/,
+    /^app:\/\//,
+    /^https?:\/\/localhost(:\d+)?$/,
+    'https://studio.apollographql.com'
+  ]
+}))
 
 initApollo(httpServer).then(async server => {
   server.applyMiddleware({
     app,
     path: '/'
-    // TODO: CORS
   })
 
   await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve))
